ab0345fdc5bc3f4393823a1312f3d77ece78627e
himanipgs <himani.chauhan@potenzaglobalsolutions.com>
2023-03-06 12:05:18 +0530

    Add project files.

---
+++ WebApplicationBranching/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplicationBranching.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
